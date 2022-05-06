adfe09fd549e624dd35ba8a52f55eb9015642f17
Bullzai <valskisv@gmail.com>
2022-05-06 23:34:54 +0100

    baseline v3 - all good

--- app/models/Station.java
+++ app/models/Station.java
@@ -53,7 +53,30 @@ public class Station extends Model
     }
 
     public int beaufort (float windSpeed) {
-
-        return 0;
+        int beaufort = 0;
+        if (1 < windSpeed && windSpeed < 5) {
+            beaufort = 1;
+        } else if (6 < windSpeed && windSpeed < 11){
+            beaufort = 2;
+        } else if (12 < windSpeed && windSpeed < 19) {
+            beaufort = 3;
+        } else if (20 < windSpeed && windSpeed < 28) {
+            beaufort = 4;
+        } else if (29 < windSpeed && windSpeed < 38) {
+            beaufort = 5;
+        } else if (39 < windSpeed && windSpeed < 49) {
+            beaufort = 6;
+        } else if (50 < windSpeed && windSpeed < 61) {
+            beaufort = 7;
+        } else if (62 < windSpeed && windSpeed < 74) {
+            beaufort = 8;
+        } else if (75 < windSpeed && windSpeed < 88) {
+            beaufort = 9;
+        } else if (89 < windSpeed && windSpeed < 102) {
+            beaufort = 10;
+        } else if (103 < windSpeed && windSpeed < 117) {
+            beaufort = 11;
+        }
+        return beaufort;
     }
 }
